615207deb3e54cf7a1502bbeeb64c6d1dfdad56c
umer <umer4167@hotmail.com>
2019-10-28 12:07:57 +0200

    winner function works

--- src/components/Board.js
+++ src/components/Board.js
@@ -7,17 +7,50 @@ export default function Board() {
 
   const handleClick = i => {
     const newSquare = [...squares];
-    console.log(newSquare);
-    newSquare[i] = xIsNext ? "X" : "O";
-    console.log(newSquare[i]);
-    setSquares(newSquare);
-    setxIsNext(!xIsNext);
+    if (newSquare[i] === null) {
+      newSquare[i] = xIsNext ? "X" : "O";
+      setSquares(newSquare);
+      setxIsNext(!xIsNext);
+      winnerDude(newSquare);
+    }
   };
+
+  function winnerDude(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ];
+
+    for (let index = 0; index < lines.length; index++) {
+      const [a, b, c] = lines[index];
+
+      if (
+        //initialy check if the sqaure[a] exist. otherwise it will immediately return winner
+        // as at first click it will compare all the empty values and return true.
+        squares[a] &&
+        squares[a] === squares[b] &&
+        squares[a] === squares[c]
+      ) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
+
   const renderSquare = i => {
     return <Square value={squares[i]} onClick={() => handleClick(i)} />;
   };
   return (
     <div>
+      <div className="currentPlayer">
+        Next player is : {xIsNext ? "X" : "O"}
+      </div>
       <div className="board-row">
         {renderSquare(0)}
         {renderSquare(1)}
